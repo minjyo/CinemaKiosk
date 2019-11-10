b71077c8d0deb5a087715576eecfb98493812c94
GeunYoungJeong <jyjg1124@gmail.com>
2019-11-10 19:00:51 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
+#include "Movie_play.h"
 #include <iostream>
+using namespace std;
 
-int main() {
+int main()
+{
+	Movie_play test;
+	test.chooseSeat();
+	test.add(10);
+	test.add(20);
+	test.add(30);
+	test.add(3, 40);
+	test.remove(0);
+	test.set(0, 100);
 
+	for (int i = 0; i < test.size(); i++)
+	{
+		cout << "data : " << test.get(i) << endl;
+	}
+	cout << "size : " << test.size() << endl;
+	cout << "isEmpty : " << test.isEmpty() << endl;
+
+	test.remove(10);
 	return 0;
 }
