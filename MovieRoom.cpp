8a5a3b3e451f4b4e5f1a535842d09889493accb1
minjyo <minjyo8823@gmail.com>
2019-11-12 22:40:12 +0900

    encoding

--- MovieRoom.cpp
+++ MovieRoom.cpp
@@ -18,7 +18,6 @@ MovieRoom::~MovieRoom() {
 //영화관의 영화 타임 테이블 출력
 void MovieRoom::printTimeTable() {
     MoviePlay* temp = head;
-    
     cout << "------------------- " << roomNumber << "관 -------------------" << endl;
     cout << "       영화 제목       영화 감독       러닝타임        " << endl;
     cout.fill(' ');
